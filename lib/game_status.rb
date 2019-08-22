7fabef81b33e43d446be4e0e050a598957eb6464
dana kim <dana0522@live.com>
2019-08-22 23:48:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ def won?(board)
     status=[position_1,position_2,position_3]
     win_index=[win_index_1,win_index_2,win_index_3]
   if status.all? {|x| x=="X"}
-    puts win_index.inspesct
+    puts win_index.inspect
   elsif status.all? {|x| x=="O"}
     puts win_index.inspect
   else
