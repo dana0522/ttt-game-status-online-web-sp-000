5a56b93295b0aa4d7604329a85adffd90c89a2fe
dana kim <dana0522@live.com>
2019-08-22 23:59:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[1,4,7],[0,3,6],[2,5
 status=[]
 win_index=[]
 def won?(board)
-  for each_combination in WIN_COMBINATIONS
+  for each_combination in WIN_COMBINATIONS:
     win_index_1=each_combination[0]
     win_index_2=each_combination[1]
     win_index_3=each_combination[2]
