100b8c2c1410644afe815e1203b62c0493c40bde
dana kim <dana0522@live.com>
2019-08-22 23:33:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,9 +20,9 @@ def won?(board)
     status=[position_1,position_2,position_3]
     win_index=[win_index_1,win_index_2,win_index_3]
   if status.all? {|x| x=="X"}
-    return win_index
+    puts win_index
   elsif status.all? {|x| x=="O"}
-    return win_index
+    puts win_index
   else
     return false
   end
