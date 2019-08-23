7d9735d6329fca4d3b4ed0bdb728a5253c604eb7
dana kim <dana0522@live.com>
2019-08-23 01:05:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,9 +6,9 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[1,4,7],[0,3,6],[2,5,8]]
 
-
 status=[]
 win_index=[]
+final=[]
 def won?(board)
   for each_combination in WIN_COMBINATIONS
     win_index_1=each_combination[0]
@@ -23,12 +23,14 @@ def won?(board)
     win_index=[win_index_1,win_index_2,win_index_3]
 
     if status.all? {|x| x=="X"} or status.all? {|x| x=="O"}
-      return win_index
+      final=win_index
     else
       false
     end
   end
+  return final
 end
+
 
 
 def full?(board)
